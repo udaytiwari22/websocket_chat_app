5b39bb4133c1577343344b91f633436ceafbb086
Uday Tiwari <udaytiwaricyef2001@gmail.com>
2024-11-22 10:34:10 +0530

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,12 @@ class ChatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Chat App',
-        theme: ThemeData(
-            brightness: Brightness.light,
-            primarySwatch: Colors.purple,
-            scaffoldBackgroundColor: const Color.fromARGB(255, 225, 131, 242)),
-        home: ChatScreen());
+      title: 'Chat App',
+      theme: ThemeData(
+          brightness: Brightness.light,
+          primarySwatch: Colors.purple,
+          scaffoldBackgroundColor: const Color.fromARGB(255, 225, 131, 242)),
+      home: ChatScreen(),
+    );
   }
 }
